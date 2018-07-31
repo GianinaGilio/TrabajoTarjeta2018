4fd2e293a4397d0fe3171708db9cdd94ecc654b7
GianinaGilio <39507205+GianinaGilio@users.noreply.github.com>
2018-07-31 17:10:23 -0300

    Update ColectivoTest.php

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -5,8 +5,8 @@ namespace TrabajoTarjeta;
 use PHPUnit\Framework\TestCase;
 
 class ColectivoTest extends TestCase {
-
+        
     public function testAlgoUtil() {
-
+        $this->assertEquals(1+1,2);
     }
 }
