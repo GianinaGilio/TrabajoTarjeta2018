7039556f1fddffc5c6b5f0ebb1a07d65c6b95817
unknown <gianinagilio@gmail.com>
2018-08-09 15:44:03 -0300

    actualizacion de montos aceptados

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -4,14 +4,16 @@ namespace TrabajoTarjeta;
 
 class Tarjeta implements TarjetaInterface {
     protected $saldo;
-
+	public $bandera;
+	
     public function recargar($monto) {
-      // Esto esta hecho mal a proposito.
-      if ($monto % 2 == 0) {
+      // Esto esta hecho mal a proposito. montos aceptados:10, 20, 30, 50, 100, 510,15 y 962,59
+      if ($monto == 10 || $monto == 20 || $monto == 30 || $monto == 50 || $monto == 100 || $monto == 510,15 || $monto == 962,59) {
         $this->saldo += $monto;
+		$bandera= TRUE;
       }
 
-      return $monto % 2 == 0;
+      return $bandera;
     }
 
     /**
