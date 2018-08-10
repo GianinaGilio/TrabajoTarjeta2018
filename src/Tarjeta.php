7f81a1278349fdb36fec2723386e2b418b3a1bdb
Sofiamonza <40038825+Sofiamonza@users.noreply.github.com>
2018-08-10 00:03:02 -0300

    Corregí la función recargar

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -1,21 +1,21 @@
 <?php
-
 namespace TrabajoTarjeta;
-
 class Tarjeta implements TarjetaInterface {
     protected $saldo;
 	public $bandera;
 	
     public function recargar($monto) {
       // Esto esta hecho mal a proposito. montos aceptados:10, 20, 30, 50, 100, 510,15 y 962,59
-      if ($monto == 10 || $monto == 20 || $monto == 30 || $monto == 50 || $monto == 100 || $monto == 510,15 || $monto == 962,59) {
+      if ($monto == 10 || $monto == 20 || $monto == 30 || $monto == 50 || $monto == 100 || $monto == 510.15 || $monto == 962.59) {
         $this->saldo += $monto;
 		$bandera= TRUE;
       }
-
+      else
+      {
+          $bandera=FALSE;
+      }
       return $bandera;
     }
-
     /**
      * Devuelve el saldo que le queda a la tarjeta.
      *
@@ -24,5 +24,4 @@ class Tarjeta implements TarjetaInterface {
     public function obtenerSaldo() {
       return $this->saldo;
     }
-
 }
