2b107a337e109fbcc8aa161f9457715b000718bc
Sofiamonza <40038825+Sofiamonza@users.noreply.github.com>
2018-08-19 02:57:42 -0300

    borre los comentarios

--- src/Boleto.php
+++ src/Boleto.php
@@ -10,7 +10,7 @@ class Boleto implements BoletoInterface {
 
     public function __construct($valor, $colectivo, $tarjeta) {
         $this->valor = $valor;
-       // $this->colectivo=$colectivo;
+    
     }
 
     /**
@@ -28,7 +28,7 @@ class Boleto implements BoletoInterface {
      * @return ColectivoInterface
      */
     public function obtenerColectivo() {
-      // return $this->colectivo;
+
     }
 
 }
