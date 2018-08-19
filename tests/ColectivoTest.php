d7bcf9996b4765423d063a5ffd69d85d741166f7
Sofiamonza <40038825+Sofiamonza@users.noreply.github.com>
2018-08-19 02:14:39 -0300

    agregue la variable $colectivo

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -5,15 +5,17 @@ namespace TrabajoTarjeta;
 use PHPUnit\Framework\TestCase;
 
 class ColectivoTest extends TestCase {
-        
+      
+
     public function testConSaldo() {
+        $colectivo = new Colectivo(); 
 
         $this->assertEquals($colectivo->pagarCon($tarjeta),$tarjeta->saldo>=14.80);
     }
 
     public function testSinSaldo() {
-        
-        $this->assertFalse();
+        $colectivo = new Colectivo(); 
+
         $this->assertEquals($colectivo->pagarCon($tarjeta), $tarjeta->saldo<14.80);
     }
 }
