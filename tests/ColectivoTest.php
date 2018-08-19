6a0dba11d767f9154077d7b6c5495a508c30c6bc
Sofiamonza <40038825+Sofiamonza@users.noreply.github.com>
2018-08-19 02:52:23 -0300

    comente las funciones

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -6,18 +6,18 @@ use PHPUnit\Framework\TestCase;
 
 class ColectivoTest extends TestCase {
       
-
+/*
     public function testpagarConSaldo() {
-      /*  $colectivo = new Colectivo(); 
+       $colectivo = new Colectivo(); 
         $tarjeta = new Tarjeta();
 
-        $this->assertEquals($colectivo->pagarCon($tarjeta),$tarjeta->obtenerSaldo()>=14.80);*/
+        $this->assertEquals($colectivo->pagarCon($tarjeta),$tarjeta->obtenerSaldo()>=14.80);
     }
 
     public function testpagarSinSaldo() {
-      /*  $colectivo = new Colectivo(); 
+       $colectivo = new Colectivo(); 
         $tarjeta = new Tarjeta();
 
-        $this->assertFalse($colectivo->pagarCon($tarjeta));*/
-    }
+        $this->assertFalse($colectivo->pagarCon($tarjeta));
+    }*/
 }
