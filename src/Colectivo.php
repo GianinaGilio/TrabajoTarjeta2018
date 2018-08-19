f44fdf81993f0c67b33c3d4feba28d98d2147a06
Sofiamonza <40038825+Sofiamonza@users.noreply.github.com>
2018-08-19 03:48:48 -0300

    Agregue el descuento de viaje pplus

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -19,6 +19,13 @@ class Colectivo implements ColectivoInterface{
     public function pagarCon(TarjetaInterface $tarjeta){
         if($tarjeta->obtenerSaldo() < 14.80)
         {
+            
+            if($tarjeta->plus>0)
+            {
+                $tarjeta->plus-=1;
+                return TRUE;
+            }
+
             return FALSE;
         }
         else
