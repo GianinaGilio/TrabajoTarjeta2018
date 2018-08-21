7574f3dc0e48809fdbb6cf237681eb4367069a8a
unknown <gianinagilio@gmail.com>
2018-08-21 14:32:52 -0300

    Creo el constructor de colectivo para conocer la linea, empresa y numero del colectivo

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -3,17 +3,27 @@
 namespace TrabajoTarjeta;
 
 class Colectivo implements ColectivoInterface{
+	public $lin;
+	public $emp;
+	public $num;
+
+	public function _construct($lin, $emp, $num) {
+        $this->lin = $lin;
+		$this->emp = $emp;
+		$this->num = $num;
+    
+    }
 
     public function linea(){
-
+	return $this->lin;
     }
 
     public function empresa(){
-        
+    return $this->emp;    
     }
 
     public function numero(){
-        
+    return $this->num;    
     }
 
     public function pagarCon(TarjetaInterface $tarjeta){
