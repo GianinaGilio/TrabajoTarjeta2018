1807438788d12d520e7c1187304cffbad304a1da
unknown <gianinagilio@gmail.com>
2018-09-06 23:19:38 -0300

    Actualizo los cambios del archivo

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -3,35 +3,38 @@
 namespace TrabajoTarjeta;
 
 class Colectivo implements ColectivoInterface{
+	public $lin;
+	public $emp;
+	public $num;
+
+	public function _construct($lin, $emp, $num) {
+        $this->lin = $lin;
+		$this->emp = $emp;
+		$this->num = $num;
+    
+    }
 
     public function linea(){
-
+	return $this->lin;
     }
 
     public function empresa(){
-        
+    return $this->emp;    
     }
 
     public function numero(){
-        
+    return $this->num;    
     }
 
     public function pagarCon(TarjetaInterface $tarjeta){
-        if($tarjeta->obtenerSaldo() < 14.80)
+        if($tarjeta->precio != 0 && $tarjeta->obtenerSaldo() < 14.80)
         {
-            
-            if($tarjeta->plus>0)
-            {
-                $tarjeta->plus-=1;
-                return TRUE;
-            }
-
-            return FALSE;
+            return $tarjeta->descuentoViajesPlus();
         }
         else
         {
-            $tarjeta->saldo -=14.80;
-            return $tarjeta->saldo; 
+            $tarjeta->descuentoSaldo();
+            return $tarjeta->obtenerSaldo(); 
         }
     }
 
