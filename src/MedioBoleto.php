44cb1ca0c6ce8a765ded67d228d97341bd1994f3
Sofiamonza <sofimonza2015@gmail.com>
2018-09-09 08:57:24 -0300

    Modifico la funcion descuentoSaldo en MedioBoleto y MedioBoleto Universitario

--- src/MedioBoleto.php
+++ src/MedioBoleto.php
@@ -5,9 +5,34 @@ class MedioBoleto extends Tarjeta {
 	public $precio=7.40;
 	protected universitario= false;
 
+	public function descuentoSaldo() {
+//Falta poner el tiempo
+			return $this->saldo-=$this->precio;
+	}
+
 }
 
 class MedioBoletoUni extends MedioBoleto {
 	protected universitario= true;
-	protected  VecesUsado= 0;
+	protected  vecesUsado= 0;
+
+
+
+	public function obtenerCantUsado(){
+		return $this->vecesUsado;
+	}
+//Falta poner el tiempo
+	public function descuentoSaldo() {
+
+	if($this->vecesUsado == 2)
+	{
+		return return $this->saldo-=($this->precio*2);
+	}
+	else{
+		$this->vecesUsado += 1;
+		return $this->saldo-=$this->precio;
+	}
+		
+	  }
+
 }
