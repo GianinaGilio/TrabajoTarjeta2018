392bf82e1714ef49d5675b40635a0a05197e6607
Sofiamonza <sofimonza2015@gmail.com>
2018-09-09 08:24:39 -0300

    Creacion de la rama y de los archivos de la clase Tiempo

--- src/MedioBoleto.php
+++ src/MedioBoleto.php
@@ -3,5 +3,11 @@ namespace TrabajoTarjeta;
 
 class MedioBoleto extends Tarjeta {
 	public $precio=7.40;
+	protected universitario= false;
+
 }
 
+class MedioBoletoUni extends MedioBoleto {
+	protected universitario= true;
+	protected  VecesUsado= 0;
+}
