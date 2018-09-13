7cd644fe0324e432168b2c055e703a3639376830
Sofiamonza <40038825+Sofiamonza@users.noreply.github.com>
2018-09-12 22:38:33 -0300

    Corrijo los errores al testear el tiempo falso

--- tests/MedioBoletoTest.php
+++ tests/MedioBoletoTest.php
@@ -16,12 +16,13 @@ class MedioBoletoTest extends TestCase {
     
     //Pago de un medio
     $tarjeta->recargar(30);
-    $this->assertTrue($tiempo->avanzar(300));
+    $tiempo->avanzar(300);
+    $this->assertEquals($tiempo->time(),300);
     $this->assertTrue($tarjeta->descuentoSaldo($tiempo));
     $this->assertEquals($tarjeta->obtenerSaldo(),22.6);
 
     //Para ver que no puede volver a pagar con medio antes de los 5 minutos.
-    $this->assertTrue($tiempo->avanzar(1));
+    $tiempo->avanzar(301);
     $this->assertEquals($tiempo->time(), 301);
     $this->assertFalse($tarjeta->descuentoSaldo($tiempo));
   }
@@ -30,11 +31,12 @@ class MedioBoletoTest extends TestCase {
   public function testPagarConMedioUni()
   { $colectivo = new Colectivo(144,"RosarioBus",5);
     $tarjeta=new MedioBoletoUni;
-    $tiempo=new TiempoFalso();
+    $tiempo=new TiempoFalso;
     
     //Pago de medio boleto universitario
     $tarjeta->recargar(30);
-    $this->assertTrue($tiempo->avanzar(350));
+    $tiempo->avanzar(350);
+    $this->assertEquals($tiempo->time(), 350);
     $this->assertTrue($tarjeta->descuentoSaldo($tiempo));
     $this->assertEquals($tarjeta->obtenerSaldo(),22.6);
     $this->assertTrue($tiempo->avanzar(650));
@@ -42,13 +44,15 @@ class MedioBoletoTest extends TestCase {
     $this->assertEquals($tarjeta->obtenerSaldo(),15.2);
 
     //Aca debería cobrarse el boleto normal
-    $this->assertTrue($tiempo->avanzar(950));
+    $tiempo->avanzar(950);
+    $this->assertEquals($tiempo->time(), 950);
     $this->assertTrue($tarjeta->descuentoSaldo($tiempo));
     $this->assertEquals($tarjeta->obtenerSaldo(),0.4);
 
     //Para ver que no puede volver a pagar con medio antes de los 5 minutos.
     $tarjeta->recargar(30);
-    $this->assertTrue($tiempo->avanzar(1000));
+    $tiempo->avanzar(1000);
+    $this->assertEquals($tiempo->time(), 1000);
     $this->assertFalse($tarjeta->descuentoSaldo($tiempo));
   }
 }
