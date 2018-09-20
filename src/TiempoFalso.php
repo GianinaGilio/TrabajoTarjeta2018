6d7c95e54b01235a5c4b9dc5a0cf59de3f5feea7
Sofiamonza <40038825+Sofiamonza@users.noreply.github.com>
2018-09-20 17:42:53 -0300

    Movi la linea que cambia la zona horaria del tiempo falso para que funcione correctamente en futuros metodos

--- src/TiempoFalso.php
+++ src/TiempoFalso.php
@@ -6,6 +6,7 @@ class TiempoFalso implements TiempoInterface{
     protected $tiempo;
 
     public function __construct($iniciarEn = 0){
+        date_default_timezone_set('America/Argentina/Buenos_Aires');
         $this->tiempo = $iniciarEn;
     } 
 
