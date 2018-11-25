121dee94a52717501e938d4dd1d2770de6890ed1
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2018-11-25 13:17:30 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -2,20 +2,20 @@
 namespace TrabajoTarjeta;
 class Tarjeta implements TarjetaInterface {
   protected $id;
-  protected $saldo=0;
-  protected $plus=2;
-  protected $precio=14.80;
-  protected $strasbordo=4.884;
-  protected $cantTrasb=1;
+  protected $saldo = 0;
+  protected $plus = 2;
+  protected $precio = 14.80;
+  protected $strasbordo = 4.884;
+  protected $cantTrasb = 1;
   public $banderaTrasb;
   protected $ultimopago;
   protected $lineaUltColectivo;
   
-	public function __construct($id){
+	public function __construct($id) {
 	  $this->id = $id;
 	}
   
-  public function cantTrasb(){
+  public function cantTrasb() {
     return $this->cantTrasb;
   }
 
@@ -24,43 +24,43 @@ class Tarjeta implements TarjetaInterface {
       // Montos aceptados:10, 20, 30, 50, 100, 510.15 y 962.59
       if ($monto == 10 || $monto == 20 || $monto == 30 || $monto == 50 || $monto == 100 || $monto == 510.15 || $monto == 962.59) {
        
-        if($monto==510.15)
+        if ($monto == 510.15)
         {
           $monto += 81.93;
         }
 
-        if($monto == 962.59)
+        if ($monto == 962.59)
         {
           $monto += 221.58;
         }
 
         $this->saldo += $monto;
 
-        if($this->plus<2)
+        if ($this->plus < 2)
         {
-          if($this->plus==0)
+          if ($this->plus == 0)
           {
-            $this->saldo-= 2 * 14.80;
+            $this->saldo -= 2 * 14.80;
           }
           else
           {
-            $this->saldo-= 14.80;
+            $this->saldo -= 14.80;
           }
         }
 
-        $this->plus=2;
-		    $this->bandera= TRUE;
+        $this->plus = 2;
+		    $this->bandera = TRUE;
       }
       else
       {
-          $this->bandera=FALSE;
+          $this->bandera = FALSE;
       }
 
       return $this->bandera;
     }
 
     
-    public function obtenerPrecio(){
+    public function obtenerPrecio() {
       return $this->precio;
     }
 
@@ -70,17 +70,17 @@ class Tarjeta implements TarjetaInterface {
     }
     
      
-    public function trasbordo(TiempoInterface $tiempo, ColectivoInterface $colectivo){
-      $dia=date("D", $tiempo->time());
-        $hora=idate("H", $tiempo->time());
+    public function trasbordo(TiempoInterface $tiempo, ColectivoInterface $colectivo) {
+      $dia = date("D", $tiempo->time());
+        $hora = idate("H", $tiempo->time());
         
-        if($this->lineaUltColectivo != $colectivo->linea() && $this->cantTrasb==0)
+        if ($this->lineaUltColectivo != $colectivo->linea() && $this->cantTrasb == 0)
         {
          
 			//	if(($tiempo->time())-($this->ultimopago) <= 3600)
 				//{	
-					$this->banderaTrasb=TRUE;
-					$this->cantTrasb=1;
+					$this->banderaTrasb = TRUE;
+					$this->cantTrasb = 1;
 					return $this->banderaTrasb;
 				}
 			
@@ -124,31 +124,31 @@ class Tarjeta implements TarjetaInterface {
         
     
       
-    $this->banderaTrasb=FALSE;
+    $this->banderaTrasb = FALSE;
     return $this->banderaTrasb;
   }
   
   
   public function descuentoSaldo(TiempoInterface $tiempo, ColectivoInterface $colectivo) {
-        $dia=date("D", $tiempo->time());
-        $hora=idate("H", $tiempo->time());
+        $dia = date("D", $tiempo->time());
+        $hora = idate("H", $tiempo->time());
         
         
-          if($this->trasbordo($tiempo,$colectivo))
+          if ($this->trasbordo($tiempo, $colectivo))
           {
                 $this->ultimopago = $tiempo->time();
                 $this->lineaUltColectivo = $colectivo->linea();
 
                 $this->saldo -= $this->strasbordo;
-                $this->cantTrasb=1;
+                $this->cantTrasb = 1;
                 return TRUE;
             
           }
            
         $this->lineaUltColectivo = $colectivo->linea();
         $this->ultimopago = $tiempo->time();
-        $this->saldo-=$this->precio;
-        $this->cantTrasb=0;
+        $this->saldo -= $this->precio;
+        $this->cantTrasb = 0;
         return TRUE;
       }
   
@@ -156,15 +156,15 @@ class Tarjeta implements TarjetaInterface {
 
 
   
-    public function obtenerID(){
+    public function obtenerID() {
       return $this->id;
     }
 
 
-    public function descuentoViajesPlus(){
-      if($this->plus>0)
+    public function descuentoViajesPlus() {
+      if ($this->plus > 0)
       {
-          $this->plus-=1;
+          $this->plus -= 1;
           return TRUE;
       }
       else
@@ -175,7 +175,7 @@ class Tarjeta implements TarjetaInterface {
     }
 
     
-    public function obtenerCantidadPlus(){
+    public function obtenerCantidadPlus() {
       return $this->plus;
     }
 }
